c68961e2a318e5efd0cbfcdcc593cb176be43c81
Asimthande <thanda@zulzi.com>
2024-05-16 21:11:33 +0200

    Added link to the github on button, added types for copy package

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,7 +4,7 @@ import ArrowLeft from "../../assets/images/icon-arrow-right";
 const Button = () => {
   return (
     <div>
-      <a className="button-container body">
+      <a className="button-container body" href="https://github.com/amajola/password-generator">
         Go to the Github Repo
         <ArrowLeft className="arrow-icon" />
       </a>
